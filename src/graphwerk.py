d69fe42fd4feedbeee264573a00faacc1ba1fabc
LivingInABubble <54778084+LivingInABubble@users.noreply.github.com>
2021-08-19 16:42:35 -0400

    my init

--- src/graphwerk.py
+++ src/graphwerk.py
@@ -1,77 +1,64 @@
-from numpy import genfromtxt
-import matplotlib.pyplot as plt
-import mpl_finance
-import numpy as np
+import os
 import uuid
 
-# Input your csv file here with historical data
+import matplotlib.pyplot as plt
+import numpy as np
+from mplfinance.original_flavor import candlestick2_ochl
+from tqdm import tqdm
 
-ad = genfromtxt('../financial_data/eurusd.csv', delimiter=',' ,dtype=str)
-pd = np.flipud(ad)
-
-buy_dir = '../data/train/buy/'
-sell_dir = '../data/train/sell/'
 
 def convolve_sma(array, period):
-    return np.convolve(array, np.ones((period,))/period, mode='valid')
+    return np.convolve(array, np.ones((period,)) / period, mode='valid')
+
 
 def graphwerk(start, finish):
-    open = []
-    high = []
-    low = []
-    close = []
-    volume = []
-    date = []
-    for x in range(finish-start):
-
-# Below filtering is valid for eurusd.csv file. Other financial data files have different orders so you need to find out
-# what means open, high and close in their respective order.
-
-        open.append(float(pd[start][1]))
-        high.append(float(pd[start][2]))
-        low.append(float(pd[start][3]))
-        close.append(float(pd[start][4]))
-        volume.append(float(pd[start][5]))
-        date.append(pd[start][0])
-        start = start + 1
+    opened, closed, high, low, volume, date = [], [], [], [], [], []
+    for x in range(start, finish):
+        # Below filtering is valid for eurusd.csv file. Other financial data files have different orders
+        # so you need to find out what means open, high and closed in their respective order.
+        opened.append(float(pd[x][1]))
+        high.append(float(pd[x][2]))
+        low.append(float(pd[x][3]))
+        closed.append(float(pd[x][4]))
+        volume.append(float(pd[x][5]))
+        date.append(pd[x][0])
 
     close_next = float(pd[finish][4])
 
-    sma = convolve_sma(close, 5)
+    sma = convolve_sma(closed, 5)
     smb = list(sma)
     diff = sma[-1] - sma[-2]
 
-    for x in range(len(close)-len(smb)):
-        smb.append(smb[-1]+diff)
+    for x in range(len(closed) - len(smb)):
+        smb.append(smb[-1] + diff)
 
     fig = plt.figure(num=1, figsize=(3, 3), dpi=50, facecolor='w', edgecolor='k')
     dx = fig.add_subplot(111)
-    #mpl_finance.volume_overlay(ax, open, close, volume, width=0.4, colorup='b', colordown='b', alpha=1)
-    mpl_finance.candlestick2_ochl(dx,open, close, high, low, width=1.5, colorup='g', colordown='r', alpha=0.5)
+    # mpl_finance.volume_overlay(ax, opened, closed, volume, width=0.4, colorup='b', colordown='b', alpha=1)
+    candlestick2_ochl(dx, opened, closed, high, low, width=1.5, colorup='g', colordown='r', alpha=0.5)
 
     plt.autoscale()
     plt.plot(smb, color="blue", linewidth=10, alpha=0.5)
     plt.axis('off')
-    comp_ratio = close_next / close[-1]
-    print(comp_ratio)
+    # comp_ratio = close_next / closed[-1]
+    # print(comp_ratio)
 
-    if close[-1] > close_next:
-            print('close value is bigger')
-            print('last value: ' + str(close[-1]))
-            print('next value: ' + str(close_next))
-            print('sell')
-            plt.savefig(sell_dir + str(uuid.uuid4()) +'.jpg', bbox_inches='tight')
+    if closed[-1] > close_next:
+        # print('close value is bigger')
+        # print('last value: ' + str(closed[-1]))
+        # print('next value: ' + str(close_next))
+        # print('sell')
+        plt.savefig(sell_dir + str(uuid.uuid4()) + '.jpg', bbox_inches='tight')
     else:
-            print('close value is smaller')
-            print('last value: '+ str(close[-1]))
-            print('next value: ' + str(close_next))
-            print('buy')
-            plt.savefig(buy_dir + str(uuid.uuid4())+'.jpg', bbox_inches='tight')
+        # print('close value is smaller')
+        # print('last value: ' + str(closed[-1]))
+        # print('next value: ' + str(close_next))
+        # print('buy')
+        plt.savefig(buy_dir + str(uuid.uuid4()) + '.jpg', bbox_inches='tight')
 
-
-    #plt.show()
-    open.clear()
-    close.clear()
+    # plt.show()
+    opened.clear()
+    closed.clear()
     volume.clear()
     high.clear()
     low.clear()
@@ -79,12 +66,18 @@ def graphwerk(start, finish):
     plt.clf()
 
 
+if __name__ == '__main__':
+    buy_dir = '../data/train/buy/'
+    if not os.path.exists(buy_dir):
+        os.makedirs(buy_dir)
 
-iter_count = int(len(pd)/4)
-print(iter_count)
-iter = 0
+    sell_dir = '../data/train/sell/'
+    if not os.path.exists(sell_dir):
+        os.makedirs(sell_dir)
 
+    # Input your csv file here with historical data
+    ad = np.genfromtxt('../financial_data/eurusd.csv', delimiter=',', dtype=str)
+    pd = np.flipud(ad)
 
-for x in range(len(pd)-4):
-   graphwerk(iter, iter+12)
-   iter = iter + 2
+    for i in tqdm(range(int(len(pd) / 2) - 6)):
+        graphwerk(i * 2, i * 2 + 12)
--- src/train-binary.py
+++ src/train-binary.py
@@ -1,120 +1,114 @@
 import os
 import sys
 
-# If you like to speed up training process with GPU, first install PlaidML and then uncomment the following line.
-# Otherwise it will fallback to tensorflow.
-
-# os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
-
 from keras import optimizers
-from keras.layers import Dropout, Flatten, Dense, Activation, BatchNormalization
+from keras.callbacks import ModelCheckpoint
+from keras.layers import Dropout, Flatten, Dense, Activation
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, Conv2D
 from keras.models import Sequential
 from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks import ModelCheckpoint
 
-DEV = False
-argvs = sys.argv
-argc = len(argvs)
 
-if argc > 1 and (argvs[1] == "--development" or argvs[1] == "-d"):
-  DEV = True
-
-if DEV:
-  epochs = 10
-else:
-  epochs = 1000
+# If you like to speed up training process with GPU, first install PlaidML and then uncomment the following line.
+# Otherwise it will fallback to tensorflow.
+# os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
 
 # BEFORE STARTING TRAINING YOU NEED TO MANUALLY TAKE 20 PERCENENT OF THE TRAINING DATA AND PUT IT INTO VALIDATION FOLDER
 # I was too lazy to do it in the code.
 
-train_data_dir = './data/train/'
-validation_data_dir = './data/validation/'
 
-# Input the size of your sample images
-img_width, img_height = 150, 150
-# Enter the number of samples, training + validation
-nb_train_samples = 13204
-nb_validation_samples = 1412
-nb_filters1 = 32
-nb_filters2 = 32
-nb_filters3 = 64
-conv1_size = 3
-conv2_size = 2
-conv3_size = 5
-pool_size = 2
-# We have 2 classes, buy and sell
-classes_num = 2
-batch_size = 128
-lr = 0.001
-chanDim =3
+def main():
+    if len(sys.argv) > 1 and (sys.argv[1] == "--development" or sys.argv[1] == "-d"):
+        epochs = 10
+    else:
+        epochs = 1000
 
-model = Sequential()
-model.add(Convolution2D(nb_filters1, conv1_size, conv1_size, border_mode ='same', input_shape=(img_height, img_width , 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(pool_size, pool_size)))
+    train_data_dir = '../data/train/'
+    validation_data_dir = '../data/validation/'
 
-model.add(Convolution2D(nb_filters2, conv2_size, conv2_size, border_mode ="same"))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(pool_size, pool_size), dim_ordering='th'))
+    # Input the size of your sample images
+    img_width, img_height = 150, 150
+    # Enter the number of samples, training + validation
+    nb_train_samples, nb_validation_samples = 13204, 1412
+    nb_filters1, nb_filters2, nb_filters3 = 32, 32, 64
+    conv1_size, conv2_size, conv3_size = 3, 2, 5
+    pool_size = 2
+    # We have 2 classes, buy and sell
+    classes_num = 2
+    batch_size = 128
+    # lr = 0.001
+    # chanDim = 3
 
-model.add(Convolution2D(nb_filters3, conv3_size, conv3_size, border_mode ='same'))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(pool_size, pool_size), dim_ordering='th'))
+    model = Sequential()
+    model.add(
+        Conv2D(nb_filters1, (conv1_size, conv1_size), input_shape=(img_height, img_width, 3), padding='same'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(pool_size, pool_size)))
 
-model.add(Flatten())
-model.add(Dense(1024))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(classes_num, activation='softmax'))
+    model.add(Conv2D(nb_filters2, (conv2_size, conv2_size), padding="same"))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(pool_size, pool_size), data_format='channels_first'))
 
-model.summary()
-model.compile(loss='categorical_crossentropy',
-                      optimizer=optimizers.rmsprop(),
-                      metrics=['accuracy'])
+    model.add(Conv2D(nb_filters3, (conv3_size, conv3_size), padding='same'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(pool_size, pool_size), data_format='channels_first'))
 
-train_datagen = ImageDataGenerator(
-    #rescale=1. / 255,
-    horizontal_flip=False)
+    model.add(Flatten())
+    model.add(Dense(1024))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(classes_num, activation='softmax'))
 
-test_datagen = ImageDataGenerator(
-    #rescale=1. / 255,
-    horizontal_flip=False)
+    model.summary()
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=optimizers.rmsprop(),
+                  metrics=['accuracy'])
 
-train_generator = train_datagen.flow_from_directory(
-    train_data_dir,
-    target_size=(img_height, img_width),
-    #shuffle=True,
-    batch_size=batch_size,
-    class_mode='categorical'
-)
+    train_datagen = ImageDataGenerator(
+        # rescale=1. / 255,
+        horizontal_flip=False)
 
-validation_generator = test_datagen.flow_from_directory(
-    validation_data_dir,
-    target_size=(img_height, img_width),
-    batch_size=batch_size,
-    #shuffle=True,
-    class_mode='categorical')
+    test_datagen = ImageDataGenerator(
+        # rescale=1. / 255,
+        horizontal_flip=False)
 
-"""
-Tensorboard log
-"""
-target_dir = "./models/weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
-if not os.path.exists(target_dir):
-  os.mkdir(target_dir)
-model.save('./src/models/model.h5')
-model.save_weights('./src/models/weights.h5')
+    train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_height, img_width),
+        # shuffle=True,
+        batch_size=batch_size,
+        class_mode='categorical'
+    )
 
-checkpoint = ModelCheckpoint(target_dir, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-callbacks_list = [checkpoint]
+    validation_generator = test_datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_height, img_width),
+        batch_size=batch_size,
+        # shuffle=True,
+        class_mode='categorical')
 
-model.fit_generator(
-    train_generator,
-    steps_per_epoch=nb_train_samples//batch_size,
-    epochs=epochs,
-    shuffle=True,
-    validation_data=validation_generator,
-    callbacks=callbacks_list,
-    validation_steps=nb_validation_samples//batch_size)
+    """
+    Tensorboard log
+    """
+    # target_dir = f"./models/weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
+    target_dir = './models/'
+    if not os.path.exists(target_dir):
+        os.mkdir(target_dir)
+    model.save('../src/models/model.h5')
+    model.save_weights('../src/models/weights.h5')
+
+    checkpoint = ModelCheckpoint(target_dir, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+    callbacks_list = [checkpoint]
+
+    model.fit_generator(
+        train_generator,
+        steps_per_epoch=nb_train_samples // batch_size,
+        epochs=epochs,
+        shuffle=True,
+        validation_data=validation_generator,
+        callbacks=callbacks_list,
+        validation_steps=nb_validation_samples // batch_size)
 
 
+if __name__ == '__main__':
+    main()
